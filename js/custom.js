2c6bcce4ca0151082e5b7f2e214703314e37fa17
Ashfahan <me@ashfahan.com>
2021-01-11 00:31:36 +0500

    hide footer and header on scroll down

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,46 @@ var popover = popoveritems.map((popoverTriggerEl) => new bootstrap.Popover(popov
 popoveritems.forEach((element) => (element.onclick = () => popover.forEach((e) => (e._element != element ? e.hide() : null))));
 
 jQuery(document).ready(($) => {
-  // header
+  const headroom = new Headroom(document.querySelector("body"), {
+    // vertical offset in px before element is first unpinned
+    offset: 0,
+    // scroll tolerance in px before state changes
+    tolerance: 0,
+    // css classes to apply
+    classes: {
+      // when element is initialised
+      initial: "headroom",
+      // when scrolling up
+      pinned: "headroom--pinned",
+      // when scrolling down
+      unpinned: "headroom--unpinned",
+      // when above offset
+      top: "headroom--top",
+      // when below offset
+      notTop: "headroom--not-top",
+      // when at bottom of scroll area
+      bottom: "headroom--bottom",
+      // when not at bottom of scroll area
+      notBottom: "headroom--not-bottom",
+      // when frozen method has been called
+      frozen: "headroom--frozen",
+      // multiple classes are also supported with a space-separated list
+      pinned: "headroom--pinned foo bar"
+    },
+    // callback when pinned, `this` is headroom object
+    onPin: function () {},
+    // callback when unpinned, `this` is headroom object
+    onUnpin: function () {},
+    // callback when above offset, `this` is headroom object
+    onTop: function () {},
+    // callback when below offset, `this` is headroom object
+    onNotTop: function () {},
+    // callback when at bottom of page, `this` is headroom object
+    onBottom: function () {},
+    // callback when moving away from bottom of page, `this` is headroom object
+    onNotBottom: function () {}
+  });
+  headroom.init();
 
   // sidebar_button-menu
   $(() => {
